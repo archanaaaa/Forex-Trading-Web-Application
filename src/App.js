e3001bfc1b835769db7f2c3636b7e6989ec43791
Harini S <harinipotter7@gmail.com>
2022-03-23 12:47:08 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,95 +1,88 @@
 import './App.css';
 import react, { useState, useEffect, Component } from 'react';
-import { Line } from 'react-chartjs-2';
-import logo from './logo.svg';
 import './App.css';
-import ReactDOM from 'react-dom';
-import Chart from 'chart.js/auto'
-
-let priceData1 = []
-let priceData2 = []
-let timeStampData = []
-
-let data = {
-  labels: timeStampData,
-  datasets: [
-    {
-      label: 'Wazirx',
-      fill: false,
-      lineTension: 0.1,
-      backgroundColor: 'rgba(75,192,192,0.4)',
-      borderColor: 'rgba(75,192,192,1)',
-      borderCapStyle: 'butt',
-      borderDash: [],
-      borderDashOffset: 0.0,
-      borderJoinStyle: 'miter',
-      pointBorderColor: 'rgba(75,192,192,1)',
-      pointBackgroundColor: '#fff',
-      pointBorderWidth: 1,
-      pointHoverRadius: 5,
-      pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-      pointHoverBorderColor: 'rgba(220,220,220,1)',
-      pointHoverBorderWidth: 2,
-      pointRadius: 1,
-      pointHitRadius: 10,
-      data: priceData1
-    },
-    {
-      label: 'Giottus',
-      fill: false,
-      lineTension: 0.1,
-      backgroundColor: 'rgba(54,162,235,0.4)',
-      borderColor: 'rgba(54,162,235,1)',
-      borderCapStyle: 'butt',
-      borderDash: [],
-      borderDashOffset: 0.0,
-      borderJoinStyle: 'miter',
-      pointBorderColor: 'rgba(54,162,235,1)',
-      pointBackgroundColor: '#fff',
-      pointBorderWidth: 1,
-      pointHoverRadius: 5,
-      pointHoverBackgroundColor: 'rgba(54,162,235,1)',
-      pointHoverBorderColor: 'rgba(220,220,220,1)',
-      pointHoverBorderWidth: 2,
-      pointRadius: 1,
-      pointHitRadius: 10,
-      data: priceData2
-    },
-
-  ]
-};
+import CanvasJSReact from "../src/canvasjs.react";
+var CanvasJSChart = CanvasJSReact.CanvasJSChart;
 
 function App() {
-  useEffect(() => {
-    fetch("/wazirx/getRate",{
-      'methods':'GET',
-      headers : {
-        'Content-Type':'application/json'
-      }
-    })
-    .then(res => res.json())
-    .then(myData1 => {
-      console.log(myData1)
-    });
-    
-    fetch('/giottus/getRate')
-    .then(res => res.json())
-    .then(myData2 => {
-      console.log(myData2)
-    });
 
+  const [data1, setData1] = useState([]);
+  const [data2, setData2] = useState([]);
+
+  
+const options = {
+  animationEnabled: true,	
+  title:{
+    text: "Exchange Rates"
+  },
+  axisY : {
+    title: "Currency Value"
+  },
+  axisX : {
+    title : "Timeline",
+    valueFormatString: "YYYY-MM-DD HH:mm:ss TT"
+  },
+  toolTip: {
+    shared: true
+  },
+  
+  data: [
+  {
+    xValueType: "dateTime",
+    type: "spline",
+    name: "WazirX",
+    showInLegend: true,
+    dataPoints: data1
+  },
+  {
+    xValueType: "dateTime",
+    type: "spline",
+    name: "coin",
+    showInLegend: true,
+    dataPoints: data2
+  }]
+}
+
+  useEffect(() => {
+    setInterval(() =>{
+      fetch("http://localhost:5000/wazirxGetRate",{
+        'methods':'GET',
+        headers : {
+          'Content-Type':'application/json',
+          'Access-Control-Allow-Origin' : 'http://localhost:5000'
+        }
+      })
+      .then(res => res.json())
+      .then(myData1 => {
+        // data1 = myData1
+        setData1([...data1,...myData1])
+        console.log(data1)
+      });
+      
+      fetch('http://localhost:5000/coinGetRate',{
+        'methods':'GET',
+        headers : {
+          'Content-Type':'application/json',
+          'Access-Control-Allow-Origin' : 'http://localhost:5000'
+        }
+      })
+      .then(res => res.json())
+      .then(myData2 => {
+        // data2 = myData2
+        setData2([...data2,...myData2])
+        console.log(data2)
+      });
+    },10000);
   });
 
-    // fetch("/wazirx")
-    // .then(res => res.text()) 
-    // .then(text => console.log(text)) 
-    // });
+
 
   return (
     <div className="App">
       <div>
-        <h2>Line Chart</h2>
-        <Line data={data} />
+        
+        <CanvasJSChart options = {options}/>
+   
       </div>
     </div>
   );
